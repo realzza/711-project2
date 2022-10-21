b41f553d340e6e7991bf527b5c3d18e3e9ab4a49
tjysdsg <tjy1018543509@163.com>
2022-10-21 18:17:07 -0400

    Add script to clean CONLL files exported from label-studio

---
+++ clean_conll.py
@@ -0,0 +1,30 @@
+"""
+Remove the extra two columns in the CONLL2003 format
+"""
+
+from argparse import ArgumentParser
+
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('input', type=str)
+    parser.add_argument('output', type=str)
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    with open(args.input, encoding='utf-8') as inf, open(args.output, 'w', encoding='utf-8') as outf:
+        for line in inf:
+            tokens = line.strip('\n').split()
+            if len(tokens) < 4:  # skip '-DOCSTART-' at the top, and keep the empty lines
+                if len(tokens) == 0:
+                    outf.write(f'\n')
+            else:
+                col1, _, _, col2 = tokens
+                outf.write(f'{col1} {col2}\n')
+
+
+if __name__ == '__main__':
+    main()
