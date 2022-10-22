1ad72592c5396b7fa6e8c464ff52cc7ae92831c2
tjysdsg <tjy1018543509@163.com>
2022-10-22 16:05:49 -0400

    Finetune SciBERT on our custom data

--- ner/finetune.sh
+++ ner/finetune.sh
@@ -1,5 +1,7 @@
 python run_ner.py \
   --model_name_or_path "allenai/scibert_scivocab_uncased" \
   --dataset_name ../our_data \
-  --output_dir ../our_data/tjy-pred.conll \
+  --output_dir tmp \
+  --do_train \
+  --do_eval \
   --do_predict
